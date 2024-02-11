1719298be98d92a75201dc1d0c8acda9be34ab29
nathen <nathens@ewisl.net>
2024-02-11 20:23:53 +0530

    Added Javascript
    Added Javascript to the code
    
    This will be the start of the programming of the services page
    
    Can only be navigated to home page and services page back and forth

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+//Buttonclick function for button in homepage
+document.querySelector('.first-page-button').addEventListener('click', function() {
+    window.location.href = '/services.html';
+});
+

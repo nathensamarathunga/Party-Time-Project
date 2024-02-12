6d7791c347f2d9005e4557c8c118713c6bd8d3d7
nathen <nathens@ewisl.net>
2024-02-12 23:34:38 +0530

    Added New features
    Added Gallery page
    
    Modified incomplete contacts page
    
    Fixed bugs with gallery page
    
    Contacts page not unsure of stability

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,9 @@ document.querySelector('.first-page-button').addEventListener('click', function(
     window.location.href = '/services.html';
 });
 
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
